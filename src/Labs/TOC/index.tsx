6b08876421a396a2451de4c227e162272bbec3b4
Jake Friedman <friedman.ja@northeastern.edu>
2025-01-09 18:28:00 -0500

    Add name

--- src/Labs/TOC/index.tsx
+++ src/Labs/TOC/index.tsx
@@ -2,22 +2,25 @@ import { Link } from "react-router";
 
 export default function TOC() {
   return (
-    <ul>
-      <li>
-        <Link to="/Labs">Labs</Link>
-      </li>
-      <li>
-        <Link to="/Labs/Lab1">Lab 1</Link>
-      </li>
-      <li>
-        <Link to="/Labs/Lab2">Lab 2</Link>
-      </li>
-      <li>
-        <Link to="/Labs/Lab3">Lab 3</Link>
-      </li>
-      <li>
-        <Link to="/Kambaz">Kambaz</Link>
-      </li>
-    </ul>
+    <div>
+      <h1>Jacob Friedman</h1>
+      <ul>
+        <li>
+          <Link to="/Labs">Labs</Link>
+        </li>
+        <li>
+          <Link to="/Labs/Lab1">Lab 1</Link>
+        </li>
+        <li>
+          <Link to="/Labs/Lab2">Lab 2</Link>
+        </li>
+        <li>
+          <Link to="/Labs/Lab3">Lab 3</Link>
+        </li>
+        <li>
+          <Link to="/Kambaz">Kambaz</Link>
+        </li>
+      </ul>
+    </div>
   );
 }
